06d3787a4bb2249e3a7c0c42b8e67ef1ea14e001
korolknopok <fedor.artem.ivanov@mail.ru>
2023-12-19 23:58:01 +0700

    1

---
+++ server/Vet_Clinic-rest/Context/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vet_Clinic_rest.Model;
+
+namespace Vet_Clinic_rest.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+           : base(options)
+        {
+
+        }
+
+        public DbSet<Client> Clients { get; set; }
+    }
+}
---
+++ server/Vet_Clinic-rest/Controllers/ClientController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using Vet_Clinic_rest.Model;
+using Vet_Clinic_rest.Context;
+using Microsoft.AspNetCore.Cors;
+
+namespace Vet_Clinic_rest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class ClientController : ControllerBase
+    {
+        private ApplicationContext _clients;
+
+        public ClientController(ApplicationContext clients)
+        {
+            _clients = clients;
+        }
+
+
+        /*private static readonly  List<Client> Clients = new(); */
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            var client = _clients.Clients;
+            return Ok(client);
+        }
+
+       /* public Client GetById(int id)
+        {
+            var clients = _clients.Clients.Find(id);
+            if (clients == null) { throw new KeyNotFoundException("User Not Found"); }
+            return clients;
+        }*/
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var emp = _clients.Clients.Find(id);
+            return Ok(emp);
+        }
+
+        
+            
+        [HttpPost]
+        public IActionResult Post([FromBody] Client model)
+        {
+
+            var userExist = _clients.Clients.Any(e => e.Name == model.Name);
+            if (userExist == true)
+            {
+                return Ok(new { Message = "User Already Created" });
+
+            }
+
+            _clients.Add(model);
+            _clients.SaveChanges();
+
+            return Ok(new { Message = "User Created" });
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var client = await _clients.Clients.FindAsync(id);
+
+            _clients.Clients.Remove(client);
+            _clients.SaveChanges();
+
+            return Ok(new { Message = "User Deleted" });
+        }
+    }
+}
