fb4745f6e8d1d056d4adb135f84252d4fce1200e
korolknopok <fedor.artem.ivanov@mail.ru>
2023-12-21 15:24:46 +0700

    Added a controller for veterinarians, namely post get delete and put

--- server/Vet_Clinic-rest/Controllers/VeterinariansController.cs
+++ server/Vet_Clinic-rest/Controllers/VeterinariansController.cs
@@ -18,5 +18,58 @@ namespace Vet_Clinic_rest.Controllers
         {
             _veterinarians = veterinarians;
         }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var veterinarians = _veterinarians.Veterinarians;
+            return Ok(veterinarians);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Veterinarians model)
+        {
+
+            var veterinarianExist = _veterinarians.Veterinarians.Any(e => e.name == model.name);
+            if (veterinarianExist == true)
+            {
+                return Ok(new { Message = "Veterinarian Already Created" });
+
+            }
+
+            _veterinarians.Add(model);
+            _veterinarians.SaveChanges();
+
+            return Ok(new { Message = "Veterinarian Created" });
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var veterinarians = await _veterinarians.Veterinarians.FindAsync(id);
+
+            _veterinarians.Veterinarians.Remove(veterinarians);
+            _veterinarians.SaveChanges();
+
+            return Ok(new { Message = "Veterinarian Deleted" });
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Veterinarians model)
+        {
+            var veterinarian = _veterinarians.Veterinarians.FirstOrDefault(e => e.id == id);
+            if (veterinarian == null)
+            {
+                return NotFound();
+            }
+            veterinarian.name = model.name;
+            veterinarian.phoneNumber = model.phoneNumber;
+            veterinarian.dateOfBirth = model.dateOfBirth;
+            veterinarian.education = model.education;
+
+            _veterinarians.SaveChanges();
+
+            return Ok(new { Message = "Veterinarian Update" });
+        }
     }
 }
