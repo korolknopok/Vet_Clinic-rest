d80f5d7500ed61e68f4892b403e116d01b16c73c
korolknopok <fedor.artem.ivanov@mail.ru>
2024-02-29 22:09:45 +0700

    Second program

--- web/src/components/ExerciseWithFunctions.tsx
+++ web/src/components/ExerciseWithFunctions.tsx
@@ -1,28 +1,87 @@
-type Operator = '+' | '-' | '*' | '/';
-function calculate(num1 : number, num2 : number, operator : Operator) : number {
-    let result: number;
-    switch (operator){
-        case "+":
-            result = num1 + num2;
-            break;
-        case "-":
-            result = num1 - num2;
-            break;
-        case "*":
-            result = num1 * num2;
-            break;
-        case "/":
-            if (num2 !== 0) {
-                return num1 / num2
-            } else {
-                throw new Error('Division by zero');
-            }
-    }
-    return result;
+// Ex1
+
+// type Operator = '+' | '-' | '*' | '/';
+// function calculate(num1 : number, num2 : number, operator : Operator) : number {
+//     let result: number;
+//     switch (operator){
+//         case "+":
+//             result = num1 + num2;
+//             break;
+//         case "-":
+//             result = num1 - num2;
+//             break;
+//         case "*":
+//             result = num1 * num2;
+//             break;
+//         case "/":
+//             if (num2 !== 0) {
+//                 return num1 / num2
+//             } else {
+//                 throw new Error('Division by zero');
+//             }
+//     }
+//     return result;
+// }
+//
+// console.log(calculate(5,5,'+'));
+// console.log(calculate(10,2,'-'));
+// console.log(calculate(10,7,'*'));
+// console.log(calculate(70,7,'/'));
+
+//Ex2
+
+interface Shape {
+    getForm() : string;
+    getId() : number;
 }
 
-console.log(calculate(5,5,'+'));
-console.log(calculate(10,2,'-'));
-console.log(calculate(10,7,'*'));
-console.log(calculate(70,7,'/'));
+class Circle implements Shape {
+    private id: number;
+    constructor(id: number) {
+        this.id = id;
+    }
+    getForm(): string {
+        return 'Circle';
+    }
+    getId(): number {
+        return this.id;
+    }
+}
 
+class Square implements Shape {
+    private id: number;
+    constructor(id:number) {
+        this.id = id;
+    }
+    getForm(): string {
+        return 'Square'
+    }
+    getId() : number {
+        return this.id;
+    }
+}
+
+class Tringle implements Shape {
+    private id: number;
+    constructor(id: number) {
+        this.id = id;
+    }
+    getForm(): string {
+        return 'Tringle';
+    }
+    getId(): number {
+        return this.id;
+    }
+}
+
+function printShapeInfo(shape:Shape): void {
+    console.log(`Id: ${shape.getId()}, Form: ${shape.getId()}`);
+}
+
+const circle = new Circle(1);
+const square = new Square(2);
+const tringle = new Tringle(3);
+
+printShapeInfo(circle);
+printShapeInfo(square);
+printShapeInfo(tringle);
