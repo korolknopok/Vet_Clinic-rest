d9e0a7b8c31b8174a068f9e12dc30d768bcc21a3
korolknopok <fedor.artem.ivanov@mail.ru>
2024-06-24 18:19:38 +0700

    feature/Add protection for un authorized users

--- web/src/components/ClientInfo.tsx
+++ web/src/components/ClientInfo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { TiDelete } from "react-icons/ti";
 import { useVeterinarians } from "./FilesRelatedToVeterinarians/VeterinariansContext.tsx";
 import SelectVet from "../routes/SelectVet.tsx";
@@ -11,20 +11,37 @@ interface IClientInfo {
         phoneNumber: string;
     };
 }
+
 const handleSelect = (id: number) => {
     console.log(`Выбран ветеринар с id: ${id}`);
 };
 
-const ClientInfo: React.FC<IClientInfo> = ({ handleDeleteData, post}) => {
+const ClientInfo: React.FC<IClientInfo> = ({ handleDeleteData, post }) => {
     const { veterinarians } = useVeterinarians();
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            setIsLoggedIn(true);
+        }
+    }, []);
+
+    const handleDelete = (id: number) => {
+        if (isLoggedIn) {
+            handleDeleteData(id);
+        } else {
+            console.error('Неавторизованные пользователи не могут удалять данные');
+        }
+    }
+
     return (
         <div>
             {post?.name}, {post?.phoneNumber}
-            <SelectVet veterinarians={veterinarians} clientId={post.id} onSelect = {handleSelect}/>
-            <TiDelete className='styleIcons' onClick={() => handleDeleteData(post.id)} />
+            <SelectVet veterinarians={veterinarians} clientId={post.id} onSelect={handleSelect} />
+            <TiDelete className='styleIcons' onClick={() => handleDelete(post.id)} />
         </div>
     );
 };
 
 export default ClientInfo;
-
