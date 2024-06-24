32ef98377f630e42c80dbf8a4c4d215c098fff40
korolknopok <fedor.artem.ivanov@mail.ru>
2024-06-24 18:49:06 +0700

    feature/Connect vet and client

--- web/src/components/FilesRelatedToVeterinarians/Veterinarians.tsx
+++ web/src/components/FilesRelatedToVeterinarians/Veterinarians.tsx
@@ -1,63 +1,54 @@
 // @ts-ignore
-import React, { useEffect, useState } from 'react';
-// @ts-ignore
 import VeterinariansInfo from './VeterinariansInfo.tsx';
 // @ts-ignore
 import VeterInfoPost from "./VeterInfoPost.tsx";
 // @ts-ignore
 import VeterInfoPut from "./VeterInfoPut.tsx";
+import {useEffect, useState} from "react";
 // @ts-ignore
-import { Veterinarians, VeterinariansApi } from "../../json/api.ts";
+import {Veterinarians, VeterinariansApi} from "../../json/api.ts";
 
 function IVeterinarians() {
     const api = new VeterinariansApi();
     const [open, setIsOpen] = useState();
     const [data, setData] = useState<Veterinarians[]>([]);
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            setIsLoggedIn(true);
-            getVetDataFromApi();
-        }
-    }, []);
 
     async function getVetDataFromApi() {
         const response = await api.apiVeterinariansGet();
-        const { data: dataResponse } = response;
+        const {data: dataResponse} = response;
         setData(dataResponse);
     }
 
+    useEffect(() => {
+        getVetDataFromApi();
+    }, []);
+
     const handleDeleteVetData = (id: number) => {
-        if (isLoggedIn) {
-            api.apiVeterinariansIdDelete(id)
-                .then(response => {
-                    if (response.status == 200) {
-                        console.log('Данные успешно удалены');
-                        getVetDataFromApi();
-                    } else {
-                        console.log('Ошибка при удалении данных');
-                    }
-                })
-                .catch(error => {
-                    console.log('Ошибка при удалении данных:', error);
-                });
-        } else {
-            console.error('Неавторизованные пользователи не могут удалять данные');
-        }
+        api.apiVeterinariansIdDelete(id)
+            .then(response => {
+                if (response.status == 200) {
+                    console.log('Данные успешно удалены');
+                    getVetDataFromApi();
+                } else {
+                    console.log('Ошибка при удалении данных');
+                }
+            })
+            .catch(error => {
+                console.log('Ошибка при удалении данных:', error);
+            });
     }
 
     return (
         <div>
-            {isLoggedIn && <VeterInfoPost post={data} />}
+            Добавить ветеринара:
+            <VeterInfoPost post={data}/>
             Список ветеринаров:
             <div>
                 {data.map(post =>
                     <div key={post.id}>
                         <VeterinariansInfo post={post} handleDeleteVetData={handleDeleteVetData} setIsOpen={setIsOpen}
                                            open={open}/>
-                        {open === post.id && isLoggedIn && (
+                        {open === post.id && (
                             <VeterInfoPut post={post}/>
                         )}
                     </div>
@@ -67,4 +58,4 @@ function IVeterinarians() {
     );
 }
 
-export default IVeterinarians;
+export default IVeterinarians
--- web/src/routes/SelectVet.tsx
+++ web/src/routes/SelectVet.tsx
@@ -1,57 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { Select, MenuItem } from '@material-ui/core';
-import { ClientApi, Veterinarian } from '../json/api.ts';
+import { Veterinarian } from "../json/api.ts";
 
 interface SelectVetProps {
     veterinarians: Veterinarian[];
     clientId: number;
+
 }
 
-const SelectVet: React.FC<SelectVetProps> = ({ veterinarians, clientId }) => {
-    const clientApi = new ClientApi();
-    const [selectedVet, setSelectedVet] = useState<Veterinarian | undefined>(undefined);
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+const SelectVet: React.FC<SelectVetProps> = ({ veterinarians, clientId, onSelect }) => {
+    const savedId = `client_${clientId}_vetId`;
+    const [selectedId, setSelectedId] = useState<number | undefined>(localStorage.getItem(savedId) ?
+        parseInt(localStorage.getItem(savedId)!) : undefined);
 
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            setIsLoggedIn(true);
-            fetchClientData();
-        }
-    }, [clientId]);
-
-    const fetchClientData = async () => {
-        try {
-            const response = await clientApi.apiClientIdGet(clientId);
-            const clientData = response.data;
-            if (clientData.veterinarians) {
-                setSelectedVet(clientData.veterinarians);
-            }
-        } catch (error) {
-            console.error('Error fetching client data:', error.message);
-        }
-    };
-
-    const handleSelectChange = async (event: React.ChangeEvent<{ value: unknown }>) => {
-        if (isLoggedIn) {
-            const vetId = event.target.value as number;
-            const vet = veterinarians.find(vet => vet.id === vetId);
-            if (vet) {
-                setSelectedVet(vet);
-                try {
-                    await clientApi.apiClientUpdateClientPost(clientId, vetId);
-                    console.log('Данные клиента успешно обновлены');
-                } catch (error) {
-                    console.error('Ошибка при обновлении данных клиента:', error.message);
-                }
-            }
-        } else {
-            console.error('Неавторизованные пользователи не могут изменять данные');
-        }
+    const handleSelectChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+        const id = event.target.value as number;
+        setSelectedId(id);
+        onSelect(id);
+        localStorage.setItem(savedId, id.toString());
     };
 
     return (
-        <Select value={selectedVet?.id || ''} onChange={handleSelectChange}>
+        <Select value = {selectedId} onChange={handleSelectChange}>
             {veterinarians.map((vet) => (
                 <MenuItem key={vet.id} value={vet.id}>
                     {vet.name}
